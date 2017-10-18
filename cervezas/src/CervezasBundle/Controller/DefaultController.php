da6ad10a85992a85d39d7532737b33431295e2d7
Ampa <ampa_margalef@hotmail.com>
2017-10-18 17:45:10 +0200

    cervezas

---
+++ cervezas/src/CervezasBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CervezasBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('CervezasBundle:Default:index.html.twig');
+    }
+}
